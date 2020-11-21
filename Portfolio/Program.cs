74a891cc34710efc0530288c51767225ec6b69ce
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-11-21 18:31:07 +0200

    Initial commit

---
+++ Portfolio/Program.cs
@@ -0,0 +1,21 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Portfolio
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio
+
+Hi, I'm Tolik Pylypchuk and this is my personal portfolio website. It's currently under construction, so hang tight
+for updates.
