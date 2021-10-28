c9cfdb0659439f0ca2c46a47e6d97190ff70b100
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-10-28 22:07:09 +0300

    Use C# 10 features

--- Portfolio/Extensions.cs
+++ Portfolio/Extensions.cs
@@ -1,61 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+namespace Portfolio;
+
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
-namespace Portfolio
+public static class Extensions
 {
-    public static class Extensions
+    [return: NotNullIfNotNull("str")]
+    public static string? Unindent(this string? str)
     {
-        [return: NotNullIfNotNull("str")]
-        public static string? Unindent(this string? str)
+        if (String.IsNullOrEmpty(str))
         {
-            if (String.IsNullOrEmpty(str))
-            {
-                return str;
-            }
-
-            var lines = str.Split(Environment.NewLine);
-            var indentation = lines.Select(line => line.Length - line.TrimStart().Length).Min();
-
-            return String.Join(
-                Environment.NewLine,
-                lines.Select(line => line[indentation..].TrimEnd().ToArray()));
+            return str;
         }
 
-        public static IEnumerable<ImmutableList<T>> Buffer<T>(this IEnumerable<T> items, int count)
+        var lines = str.Split(Environment.NewLine);
+        var indentation = lines.Select(line => line.Length - line.TrimStart().Length).Min();
+
+        return String.Join(
+            Environment.NewLine,
+            lines.Select(line => line[indentation..].TrimEnd().ToArray()));
+    }
+
+    public static IEnumerable<ImmutableList<T>> Buffer<T>(this IEnumerable<T> items, int count)
+    {
+        if (items is null)
         {
-            if (items is null)
-            {
-                throw new ArgumentNullException(nameof(items));
-            }
+            throw new ArgumentNullException(nameof(items));
+        }
 
-            if (count <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(count), "The count must be positive");
-            }
+        if (count <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count), "The count must be positive");
+        }
 
-            var list = ImmutableList.CreateBuilder<T>();
-            int currentCount = 0;
+        var list = ImmutableList.CreateBuilder<T>();
+        int currentCount = 0;
 
-            foreach (var item in items)
-            {
-                list.Add(item);
-                currentCount++;
+        foreach (var item in items)
+        {
+            list.Add(item);
+            currentCount++;
 
-                if (currentCount % count == 0)
-                {
-                    yield return list.ToImmutable();
-                    currentCount = 0;
-                    list.Clear();
-                }
-            }
-
-            if (currentCount != 0)
+            if (currentCount % count == 0)
             {
                 yield return list.ToImmutable();
+                currentCount = 0;
+                list.Clear();
             }
+        }
+
+        if (currentCount != 0)
+        {
+            yield return list.ToImmutable();
         }
     }
 }
--- Portfolio/Models.cs
+++ Portfolio/Models.cs
@@ -1,6 +1,5 @@
-namespace Portfolio
-{
-    public record AccountLink(string Name, string Icon, string Url);
+namespace Portfolio;
 
-    public record Technology(string Name, string Description, string IconLink);
-}
+public record AccountLink(string Name, string Icon, string Url);
+
+public record Technology(string Name, string Description, string IconLink);
