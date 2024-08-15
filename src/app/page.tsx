a58172a80cc18f57416654a73277f596a9ce18aa
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-08-15 23:08:30 +0300

    Create a new portfolio website using Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <p>Hello world!</p>
+      <p>
+        You have found my portfolio website. It&apos;s really barebones, because
+        I&apos;ve started fully rewriting it using React and Next.js.
+      </p>
+      <p>Hopefully, it will have more content soon!</p>
+    </>
+  );
+}
