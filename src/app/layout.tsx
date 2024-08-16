a7251e4121b7999776be4680a54e7a0c89dfcb10
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-08-16 23:10:28 +0300

    Add a concept for the front page
    This is absolutely not done yet and I'm basically just finguring it out
    as I go, but this looks like a good first draft.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio | Tolik Pylypchuk",
+  title: "Tolik's Website",
   description: "Tolik Pylypchuk's portfolio website",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import author from "../../public/me.jpg";
+
 export default function Home() {
   return (
-    <>
-      <p>Hello world!</p>
-      <p>
-        You have found my portfolio website. It&apos;s really barebones, because
-        I&apos;ve started fully rewriting it using React and Next.js.
-      </p>
-      <p>Hopefully, it will have more content soon!</p>
-    </>
+    <main className={styles.headerWrapper}>
+      <header className={styles.header}>
+        <div className={styles.nameWrapper}>
+          <h1 className={styles.name}>Hi, I&apos;m Tolik Pylypchuk</h1>
+          <h2 className={styles.subtext}>A software developer</h2>
+        </div>
+        <div className={styles.photoWrapper}>
+          <Image className={styles.photo} src={author} alt="Me" />
+        </div>
+      </header>
+    </main>
   );
 }
